258f05924ba632f339fa3c3cc720b483237541f1
Ian Cenry <iancenry@gmail.com>
2023-04-17 23:47:50 +0300

    edit Van component

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -2,33 +2,38 @@ import { useEffect, useState } from 'react'
 import beachbum from '../assets/images/beach-bum.png'
 import { Link } from 'react-router-dom'
 
-const Van = () => (
+const Van = ({name, price, image, type}) => (
         <div className="vans">
             <div className="van-card">
-                <img src={beachbum} alt="van image" width='100%'  />
+                <img src={`/src/assets/images/${image}`} alt="van image" width='100%'  />
+                {/* <img src={'/src/assets/images/about-hero.png'} alt="van image" width='100%'  /> */}
                 <div className="van-details">
-                    <h2>Modest Explorer</h2>
-                    <p className='van-price'><span>$60</span> /day</p>
+                    <h2>{name}</h2>
+                    <p className='van-price'><span>${price}</span> /day</p>
                 </div>
-                <Link className="van-button">Simple</Link>
+                <Link className="van-button">{type}</Link>
             </div>
         </div>
 
 )
 
-const Vans = () => {    
+const Vans = () => {  
+    const [vans, setVans] = useState([]) 
 
     useEffect(() => {
         fetch('api/vans')
             .then(res => res.json())
-            .then(data => setVans(data))
+            .then(data => setVans(data.vans))
     }, [])
 
+    let vansAray = vans.map( van => (
+        <Van key={van.id} name={van.name} price={van.price} image={van.imageUrl} type={van.type} />
+    ))
 
   return (
     <div>
         <h1>Explore our van options</h1>
-        <Van />
+        {vansAray}
 
     </div>
   )
