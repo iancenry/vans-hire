fdfc2a4b6a3b0e81efbe1f95efa2bde1496f90be
Ian Cenry <iancenry@gmail.com>
2023-04-18 02:10:06 +0300

    update Vans.jsx

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -2,19 +2,27 @@ import { useEffect, useState } from 'react'
 import beachbum from '../assets/images/beach-bum.png'
 import { Link } from 'react-router-dom'
 
-const Van = ({name, price, image, type}) => (
-        <div className="vans">
-            <div className="van-card">
-                <img src={`/src/assets/images/${image}`} alt="van image" width='100%'  />
-                <div className="van-details">
-                    <h2>{name}</h2>
-                    <p className='van-price'><span>${price}</span> /day</p>
-                </div>
-                <Link className="van-button">{type}</Link>
+const Van = ({name, price, image, type, id}) => {
+    let buttonColor; 
+    if(type === 'luxury') buttonColor = '#161616'
+    else if(type === 'simple') buttonColor = '#E17654'
+    else if(type === 'rugged') buttonColor = '#115E59'
+
+    return (    
+    <Link to={`/vans/${id}`}>
+        <div className="van-card">
+            <img src={`/src/assets/images/${image}`} alt="van image" width='100%'  />
+            <div className="van-details">
+                <h2>{name}</h2>
+                <p className='van-price'><span>${price}</span> /day</p>
             </div>
+            <Link className="van-button" style={{backgroundColor: buttonColor, color: '#fff'}}>{type}</Link>
         </div>
+    </Link>    
+    
 
 )
+}
 
 const Vans = () => {  
     const [vans, setVans] = useState([]) 
@@ -25,14 +33,15 @@ const Vans = () => {
             .then(data => setVans(data.vans))
     }, [])
 
-    let vansAray = vans.map( van => (
-        <Van key={van.id} name={van.name} price={van.price} image={van.imageUrl} type={van.type} />
+    let vansArray = vans.map( van => (
+        <Van key={van.id} id={van.id} name={van.name} price={van.price} image={van.imageUrl} type={van.type} />
     ))
 
   return (
     <div>
         <h1>Explore our van options</h1>
-        {vansAray}
+        <div className="vans">{vansArray}</div>
+        
 
     </div>
   )
