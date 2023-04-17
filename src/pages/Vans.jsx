4dc9328c221abf5c78a04bcbac362b4f39517752
Ian Cenry <iancenry@gmail.com>
2023-04-17 22:55:44 +0300

    add van card structure

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,21 @@
+import beachbum from '../assets/images/beach-bum.png'
+import { Link } from 'react-router-dom'
 const Vans = () => {
   return (
-    <div>Vans</div>
+    <div>
+        <h1>Explore our van options</h1>
+        <div className="vans">
+            <div className="van-card">
+                <img src={beachbum} alt="van image" width='100%'  />
+                <div className="van-details">
+                    <h2>Modest Explorer</h2>
+                    <p className='van-price'><span>$60</span> /day</p>
+                </div>
+                <Link className="van-button">Simple</Link>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
