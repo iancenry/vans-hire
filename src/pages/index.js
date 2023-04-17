b6dd6dda608b8aa66e6a07f9e6d2cc79504f52ad
Ian Cenry <iancenry@gmail.com>
2023-04-18 02:10:43 +0300

    add van details page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Home from "./Home";
 import About from "./About";
 import Vans from "./Vans";
+import VanDetail from "./VanDetail";
 
 export {
-  Home, About, Vans
+  Home, About, Vans, VanDetail
 };
