a34dd5722539ec947cb3b8aaf992bbf6bba101c0
Ian Cenry <iancenry@gmail.com>
2023-04-20 00:28:25 +0300

    style van-button

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -4,19 +4,25 @@ import { useParams } from "react-router-dom"
 const VanDetail = () => {
   const params = useParams()
   const [van, setVan] = useState(null)
+  let buttonColor; 
+    
 
   useEffect(() => {
       fetch(`/api/vans/${params.id}`)
           .then(res => res.json())
           .then(data => setVan(data.vans))
-  }, [params.id])
+  }, [params.id]) 
+
+  if(van?.type === 'luxury') buttonColor = '#161616'
+    else if(van?.type === 'simple') buttonColor = '#E17654'
+    else if(van?.type === 'rugged') buttonColor = '#115E59'
 
   return (
       <div className="van-detail-container">
           {van ? (
               <div className="van-detail">
                   <img src={`/src/assets/images/${van.imageUrl}`} />
-                  <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                  <i className={`van-button`} style={{backgroundColor: buttonColor, color: '#fff'}}>{van.type}</i>
                   <h2>{van.name}</h2>
                   <p className="van-price"><span>${van.price}</span>/day</p>
                   <p>{van.description}</p>
