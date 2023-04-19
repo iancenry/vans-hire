692318034ce7f677f005c9d2a0dcdc6ad6acc2cf
Ian Cenry <iancenry@gmail.com>
2023-04-19 23:11:18 +0300

    export more pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,10 @@ import Home from "./Home";
 import About from "./About";
 import Vans from "./Vans/Vans";
 import VanDetail from "./Vans/VanDetail";
+import Dashboard from "./Host/Dashboard"
+import Income from "./Host/Income"
+import Reviews from "./Host/Reviews"
 
 export {
-  Home, About, Vans, VanDetail
+  Home, About, Vans, VanDetail, Dashboard, Income, Reviews
 };
