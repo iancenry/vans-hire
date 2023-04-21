b6a53422272545165c72f697dc698150dc193a4f
Ian Cenry <iancenry@gmail.com>
2023-04-22 00:17:09 +0300

    add footer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Header from "./Header";
 import Layout from "./Layout";
 import HostLayout from "./HostLayout";
+import Footer from './Footer'
 
 export {
-  Header, Layout, HostLayout
+  Header, Layout, HostLayout, Footer
 };
