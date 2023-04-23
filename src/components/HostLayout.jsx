4da122a97efcf605d2c9f8c461cf41f2382d5072
Ian Cenry <iancenry@gmail.com>
2023-04-23 22:01:56 +0300

    add NavLinks

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,12 +1,18 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, NavLink } from "react-router-dom"
 
 const HostLayout = () => {
+  const activeStyle = {
+    fontWeight: 'bold',
+    textDecoration: 'underline',
+    color: '#161616'
+  }
+
   return (
     <>
         <nav className="host-nav">
-            <Link to="/host">Dashboard</Link>
-            <Link to="/host/income">Income</Link>
-            <Link to="/host/reviews">reviews</Link>
+            <NavLink to="/host">Dashboard</NavLink>
+            <NavLink to="/host/income" style={({isActive}) => isActive ? activeStyle : null}>Income</NavLink>
+            <NavLink to="/host/reviews" style={({isActive}) => isActive ? activeStyle : null}>reviews</NavLink>
         </nav>
         <Outlet />
     </>
