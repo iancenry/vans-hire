7e929208bc73d749a3d1a5973e1eef8b2ab1c95d
Ian Cenry <iancenry@gmail.com>
2023-04-23 23:16:18 +0300

    update Navlinks

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet, NavLink } from "react-router-dom"
 
 const HostLayout = () => {
-  const activeStyle = {
+  const activeStyles = {
     fontWeight: 'bold',
     textDecoration: 'underline',
     color: '#161616'
@@ -10,9 +10,10 @@ const HostLayout = () => {
   return (
     <>
         <nav className="host-nav">
-            <NavLink to="/host">Dashboard</NavLink>
-            <NavLink to="/host/income" style={({isActive}) => isActive ? activeStyle : null}>Income</NavLink>
-            <NavLink to="/host/reviews" style={({isActive}) => isActive ? activeStyle : null}>reviews</NavLink>
+            <NavLink to="/host" end style={({isActive}) => isActive ? activeStyles : null}>Dashboard</NavLink>
+            <NavLink to="/host/income" style={({isActive}) => isActive ? activeStyles : null}>Income</NavLink>
+            <NavLink to="/host/vans" style={({isActive}) => isActive ? activeStyles : null}>Vans</NavLink>
+            <NavLink to="/host/reviews" style={({isActive}) => isActive ? activeStyles : null}>reviews</NavLink>
         </nav>
         <Outlet />
     </>
