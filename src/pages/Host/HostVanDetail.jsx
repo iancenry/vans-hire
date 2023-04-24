6e50de59880f2f1f28c66ccf074b23e6590241ba
Ian Cenry <iancenry@gmail.com>
2023-04-24 12:12:32 +0300

    fetch and display host van details

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 const HostVanDetail = () => {
+  const {id} = useParams()
+  const [currentVan, setCurrentVan] = useState([])
+  
+  useEffect(()=>{
+    fetch(`/api/host/vans/${id}`)
+      .then(res => res.json())
+      .then(data => setCurrentVan(data.vans))
+  }, [])
+
+  //TODO replace with an actual loading state; this is a shortcut
+  if(!currentVan) return <h1>Loading...</h1>
+
   return (
-    <div>HostVanDetail</div>
+    <div>
+      <img src={`/src/assets/images/${currentVan.imageUrl}`} alt={currentVan.name} width={150} />   
+      <h2>{currentVan.name}</h2>         
+      <p>${currentVan.price}</p>         
+      <p>{currentVan.type}</p>         
+    </div>
   )
 }
 
