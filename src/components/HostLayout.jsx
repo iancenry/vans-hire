627e333ed6700eafdf5230a62c107962c19aef45
Ian Cenry <iancenry@gmail.com>
2023-05-01 23:16:57 +0300

    minor fix

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -7,7 +7,7 @@ const HostLayout = () => {
             <NavLink to="." end className={({isActive}) => isActive ? "active-link" : null}>Dashboard</NavLink>
             <NavLink to="income" className={({isActive}) => isActive ? "active-link" : null}>Income</NavLink>
             <NavLink to="vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
-            <NavLink to="reviews" className={({isActive}) => isActive ? "active-link" : null}>reviews</NavLink>
+            <NavLink to="reviews" className={({isActive}) => isActive ? "active-link" : null}>Reviews</NavLink>
         </nav>
         <Outlet />
     </>
