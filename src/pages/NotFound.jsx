3a82305ca9ace3111aaed836f9029643e8f4643d
Ian Cenry <iancenry@gmail.com>
2023-05-02 23:13:46 +0300

    update 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="NotFound-page">
+        <h1>Sorry, the page you were looking for was not found</h1>
+        <Link to="/" className="link-button">Return to home</Link>
+    </div>
   )
 }
+
+export default NotFound
