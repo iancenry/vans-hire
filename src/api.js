8893c5c209be28c80ab2380223313c40823e051b
Ian Cenry <iancenry@gmail.com>
2023-05-03 23:13:05 +0300

    add error handling: getVans()

--- src/api.js
+++ src/api.js
@@ -1,5 +1,16 @@
+// file containes all library/utility funcs - has all interface with api
+
 export async function getVans(){
     const res = await fetch("/api/vans")
+    if(!res.ok) {
+        throw Error({
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        })
+    }
     const data = await res.json()
     return data.vans
 }
+
+//5:03:13
