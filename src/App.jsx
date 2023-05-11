4231752222b396d93048eced863ed941f0be3c55
Ian Cenry <iancenry@gmail.com>
2023-05-11 23:28:24 +0300

    update App.jsx to data router

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import './App.css'
 import './server'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
 import { Home, About, Vans, VanDetail, Dashboard, Income, Reviews, HostVans, HostVanDetail, HostVanInfo, HostVanPhotos, HostVanPricing, NotFound } from './pages'
 import { Layout, HostLayout } from './components'
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route element={<Layout />}>
+    <Route path='/' element={<Home />} />
+    <Route path='about' element={<About />} />
+    <Route path='vans' element={<Vans />} />
+    <Route path='vans/:id' element={<VanDetail />} />
+            
+    <Route path='host' element={<HostLayout />} >
+      <Route index element={<Dashboard />} />
+      <Route path="income" element={<Income />} />
+      <Route path="reviews" element={<Reviews />} />
+      <Route path="vans" element={<HostVans />} />
+
+      <Route path="vans/:id" element={<HostVanDetail />} >
+        <Route index element={<HostVanInfo />} />
+        <Route path="pricing" element={<HostVanPricing />} />
+        <Route path="photos" element={<HostVanPhotos />} />
+      </Route>
+    </Route>
+    <Route path="*" element={<NotFound />} />
+  </Route> 
+
+))
 
 function App() {
   return (
-      <Router> 
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path='/' element={<Home />} />
-            <Route path='about' element={<About />} />
-            <Route path='vans' element={<Vans />} />
-            <Route path='vans/:id' element={<VanDetail />} />
-            
-            <Route path='host' element={<HostLayout />} >
-              <Route index element={<Dashboard />} />
-              <Route path="income" element={<Income />} />
-              <Route path="reviews" element={<Reviews />} />
-              <Route path="vans" element={<HostVans />} />
-
-              <Route path="vans/:id" element={<HostVanDetail />} >
-                <Route index element={<HostVanInfo />} />
-                <Route path="pricing" element={<HostVanPricing />} />
-                <Route path="photos" element={<HostVanPhotos />} />
-              </Route>
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Router>
-    
+      <RouterProvider router={router} />    
   )
 }
 
