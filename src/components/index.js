e389807dc535c8fb12db9e5ff8e7af5d52a63119
Ian Cenry <iancenry@gmail.com>
2023-05-15 00:00:11 +0300

    new imports

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Header from "./Header";
 import Layout from "./Layout";
 import HostLayout from "./HostLayout";
 import Footer from './Footer'
+import Error from "./Error";
 
 export {
-  Header, Layout, HostLayout, Footer
+  Header, Layout, HostLayout, Footer, Error
 };
