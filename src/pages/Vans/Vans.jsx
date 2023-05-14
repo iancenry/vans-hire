75d93fc28082e2adc51b9c7e069feaa0eb01657b
Ian Cenry <iancenry@gmail.com>
2023-05-14 23:42:47 +0300

    replace useEffect with loader

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -8,32 +8,11 @@ export function loader(){
 }
 
 const Vans = () => {  
-    // const [vans, setVans] = useState([]) 
-    const [isLoading, setIsLoading] = useState(false) 
+    const vans = useLoaderData()
     const [error, setError] = useState(null) 
-
+    
     const [searchParams, setSearchParams] = useSearchParams()
     const typeFilter = searchParams.get("type")
-
-    const vans = useLoaderData()
-
-    // useEffect(() => {
-    //     async function loadVans(){
-    //         setIsLoading(true)
-    //         try {
-    //             const data = await getVans()
-    //             setVans(data)    
-    //         } catch (err) {
-    //             setError(err)                
-    //         }finally{
-    //             setIsLoading(false)
-    //         }
-                       
-    //         setIsLoading(false)
-    //     }  
-                
-    //     loadVans()
-    // }, [])
 
     //if there is a type filter, vans that match condition are added to var else add all vans
     const displayedVans = typeFilter ?  vans.filter(van => van.type.toLowerCase() === typeFilter ) : vans
@@ -43,9 +22,7 @@ const Vans = () => {
     ))
 
     function serverResult(){
-        if(isLoading){
-            return  <div className='loader'> <img src="/src/assets/images/clouds-spinner.gif" alt="" /> </div>
-        }else if(error){
+        if(error){
             return <div className='loader'> <h1>Server Error, Try Again Later. {error.message}</h1></div>
         }else{
             return <div className="vans">{vansArray}</div> 
@@ -69,3 +46,7 @@ const Vans = () => {
 export default Vans
 
 // TODO apply error and loading to VanDetail, HostVans, HostVanDetail
+
+// if(isLoading){
+//     return  <div className='loader'> <img src="/src/assets/images/clouds-spinner.gif" alt="" /> </div>
+// }
