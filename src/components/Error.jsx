042a6d729208d2076a1f6e8c2d87e3827a5f6c16
Ian Cenry <iancenry@gmail.com>
2023-05-15 19:27:24 +0300

    useRouteError to access thrown error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,13 @@
+import { useRouteError } from "react-router-dom"
+
 const Error = () => {
+  const error = useRouteError()
+
   return (
-    <div className='loader'> <h1>Server Error, Try Again Later.</h1></div>
+    <>
+      <div className='error-text'> <h1>Server Error, {error?.message}. Try Again Later. </h1></div>
+      <pre className="error-text">{error?.status} - {error?.statusText}</pre>
+    </>
   )
 }
 
