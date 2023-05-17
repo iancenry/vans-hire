ccc39e0d0aef061b5bac74cdd56a08cc198010a9
Ian Cenry <iancenry@gmail.com>
2023-05-17 21:24:20 +0300

    update server

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ createServer({
         this.namespace = "api"
         this.logging = false
         //manually set loading time
-        this.timing = 1000
+        // this.timing = 1000
 
         this.get("/vans", (schema, request) => {
             return new Response(400, {}, {error: "Error fetching data"})
