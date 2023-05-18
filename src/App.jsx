ab1b76ffe597929c6a723b66178ffe3ff82a8913
Ian Cenry <iancenry@gmail.com>
2023-05-18 22:19:50 +0300

    add loaders to protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path='vans/:id' element={<VanDetail />} />
             
     <Route path='host' element={<HostLayout />} >
-      <Route index element={<Dashboard />} />
-      <Route path="income" element={<Income />} />
-      <Route path="reviews" element={<Reviews />} />
-      <Route path="vans" element={<HostVans />} />
+      <Route index element={<Dashboard />} loader={async() => {return null}}/>
+      <Route path="income" element={<Income />} loader={async() => {return null}}/>
+      <Route path="reviews" element={<Reviews />} loader={async() => {return null}}/>
+      <Route path="vans" element={<HostVans />} loader={async() => {return null}}/>
 
-      <Route path="vans/:id" element={<HostVanDetail />} >
-        <Route index element={<HostVanInfo />} />
-        <Route path="pricing" element={<HostVanPricing />} />
-        <Route path="photos" element={<HostVanPhotos />} />
+      <Route path="vans/:id" element={<HostVanDetail />} loader={async() => {return null}}>
+        <Route index element={<HostVanInfo />} loader={async() => {return null}}/>
+        <Route path="pricing" element={<HostVanPricing />} loader={async() => {return null}}/>
+        <Route path="photos" element={<HostVanPhotos />} loader={async() => {return null}}/>
       </Route>
     </Route>
     <Route path="*" element={<NotFound />} />
