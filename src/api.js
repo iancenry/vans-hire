dc7adf53aef0b79cdceadd32c7c20af36a603c45
Ian Cenry <iancenry@gmail.com>
2023-05-18 22:51:03 +0300

    add getHostVans api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 // library/utility functions - has all interface with apis
 
-//can take id to give one van/not and gets all vans
 export async function getVans(id){
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
@@ -16,3 +15,21 @@ export async function getVans(id){
     const data = await res.json()
     return data.vans
 }
+
+
+export async function getHostVans(id){
+    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+    const res = await fetch(url)
+    if(!res.ok) {
+        const errorObject = {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+        
+        throw errorObject
+    }
+    const data = await res.json()
+    return data.vans
+}
+
