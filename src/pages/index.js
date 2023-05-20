4a0db5a90ab409f9195164e86632bf06b139727c
Ian Cenry <iancenry@gmail.com>
2023-05-20 19:16:09 +0300

    rename imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,14 +11,14 @@ import HostVanInfo from "./Host/HostVanInfo"
 import HostVanPhotos from "./Host/HostVanPhotos"
 import HostVanPricing from "./Host/HostVanPricing"
 import NotFound from "./NotFound"
-import { loader } from "./Vans/Vans";
-import { loader2 } from "./Vans/VanDetail";
-import { loader3 } from "./Host/HostVans";
-import { loaderHVD } from "./Host/HostVanDetail";
+import { vansLoader } from "./Vans/Vans";
+import { vanDetailLoader } from "./Vans/VanDetail";
+import { hostVansLoader } from "./Host/HostVans";
+import { hostVanDetailLoader } from "./Host/HostVanDetail";
 import Login from "./Login";
 
 export {
   Home, About, Vans, VanDetail, Dashboard, Income, Reviews, HostVans, HostVanDetail,
   HostVanInfo, HostVanPhotos, HostVanPricing, NotFound,
-  loader, loader2, loader3, loaderHVD, Login
+  vansLoader, vanDetailLoader, hostVansLoader, hostVanDetailLoader, Login
 };
