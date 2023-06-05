9c3bdd3822d1e89b78d295eddecfde45ce1e3689
Ian Cenry <iancenry@gmail.com>
2023-06-06 00:25:16 +0300

    add utility file

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+import { redirect } from "react-router-dom";
+
+export async function requireAuth(){
+    const isLoggedIn = false;
+
+    if(!isLoggedIn) {
+        // return redirect("/login") - OG line below is due to mirage js not working well with latest react
+        const response  = redirect("/login") 
+        response.body = true
+        throw response
+    }
+    return null
+}
