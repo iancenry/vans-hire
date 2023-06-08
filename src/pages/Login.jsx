07d4d374a541793fde9494c97db5d98ed2ff6d09
Ian Cenry <iancenry@gmail.com>
2023-06-08 21:23:32 +0300

    add login loader: display message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useLoaderData } from "react-router-dom"
+
+export function loginLoader({request}){
+    return new URL(request.url).searchParams.get('msg')
+}
 
 const Login = () => {
     const [loginFormData, setLoginFormData] = useState({email : "", password: ""})
+    const message = useLoaderData()
 
     function handleChange(e){
         const {name, value} = e.target;
@@ -17,6 +22,7 @@ const Login = () => {
     return (
         <div className="login-container">
             <h1>Sign In</h1>
+            {message && <h3 className="red">{message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input type="email" name="email" onChange={handleChange} value={loginFormData.email} placeholder="Email Address" />
                 <input type="password" name="password" onChange={handleChange} value={loginFormData.password} placeholder="Password" />
