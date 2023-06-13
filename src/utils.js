ffcd968ba9e8f7f5e40b2f7ad281a20718c5424c
Ian Cenry <iancenry@gmail.com>
2023-06-13 11:40:13 +0300

    get isLoggedIn from local storage

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,12 @@
 import { redirect } from "react-router-dom";
 
 export async function requireAuth(){
-    const isLoggedIn = false;
+    // needs some authentication
+    // check if already logged in    
+    const isLoggedIn = localStorage.getItem('loggedIn');
 
     if(!isLoggedIn) {
-        // return redirect("/login") - OG line below is due to mirage js not working well with latest react
-        const response  = redirect("/login?msg=You must log in first") 
-        response.body = true
-        throw response
+        throw redirect("/login?msg=You must log in first") 
     }
     return null
 }
