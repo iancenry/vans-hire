d827eded9031ace4f874656329d199218473ba99
Ian Cenry <iancenry@gmail.com>
2023-06-16 23:01:34 +0300

    access request objects's pathname for redirection

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,14 @@
 import { redirect } from "react-router-dom";
 
-export async function requireAuth(){
+export async function requireAuth(request){
+    const pathname =  new URL(request.url).pathname
     // needs some authentication
     // check if already logged in    
     const isLoggedIn = localStorage.getItem('loggedIn');
 
     if(!isLoggedIn) {
         // return redirect("/login") - OG line below is due to mirage js not working well with latest react
-        const response  = redirect("/login?msg=You must log in first") 
+        const response  = redirect(`/login?msg=You must log in first.&redirectTo=${pathname}`) 
         response.body = true
         throw response
     }
