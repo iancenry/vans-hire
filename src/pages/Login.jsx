bfc9622eaa118a40e09e96973053612df41f580f
Ian Cenry <iancenry@gmail.com>
2023-06-16 20:37:16 +0300

    refractor: useNavigation hook

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,16 @@
-import { useState } from "react"
-import { Form, useLoaderData, redirect, useActionData } from "react-router-dom"
+import { Form, useLoaderData, redirect, useActionData, useNavigation } from "react-router-dom"
 import { loginUser } from '../api'
 
 export function loginLoader({request}){
-    // since loaders always run before route transitions, we check if user already logged in to prevent access to login page and redirect to /host
     if(localStorage.getItem('loggedIn')) return redirect('/host')
-    // pull message from url search parameter
     return new URL(request.url).searchParams.get('msg')
 }
 
 export async function action({ request }){
-    // get data from Form component
     const formData = await request.formData()
     const email = formData.get('email')
     const password = formData.get("password")
     try {
-        // send data and await response from server api
         const data = await loginUser({email, password})
         localStorage.setItem('loggedIn', true)
         return redirect("/host")
@@ -27,6 +22,7 @@ export async function action({ request }){
 const Login = () => {
     const errorMessage = useActionData()
     const message = useLoaderData()
+    const navigation = useNavigation()
 
     return (
         <div className="login-container">
@@ -36,7 +32,7 @@ const Login = () => {
             <Form method="post"  className="login-form">
                 <input type="email" name="email" placeholder="Email Address" />
                 <input type="password" name="password"placeholder="Password" />
-                <button className="login-btn" disabled={status === "submitting"}>{status === "submitting" ? "Logging In..." : "Log In"}</button>
+                <button className="login-btn" disabled={navigation.state === "submitting"}>{navigation.state === "submitting" ? "Logging In..." : "Log In"}</button>
             </Form>
         </div>
     )
