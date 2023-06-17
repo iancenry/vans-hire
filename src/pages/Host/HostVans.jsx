2bf8c7dfe830c9e1c20eedd9f28eb75f8aed1537
Ian Cenry <iancenry@gmail.com>
2023-06-17 22:51:50 +0300

    refractor to defer, Await, Suspense: HostVans

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,34 +1,47 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { Await, Link, defer, useLoaderData } from "react-router-dom"
 import { getHostVans } from "../../api"
 import  { requireAuth } from "../../utils"
+import { Suspense } from "react"
 
 export async function hostVansLoader({request}) {
   //ensure that it runs completely before we get the list of vans
   await requireAuth(request)
-  return getHostVans()
+  return defer({hostvans : getHostVans()})
 }
 
 const HostVans = () => {
-  const hostvans = useLoaderData() 
+  const hostvansLoaderPromise = useLoaderData() 
 
-  const hostVansEls = hostvans.map(van => (
-    <Link to={van.id}  key={van.id} className="host-van-link-wrapper">
-      <div className="host-van-single"  key={van.id}>
-        <img src={`/src/assets/images/${van.imageUrl}`} alt={van.name} />
-        <div className="host-van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
-        </div>
-      </div>
-    </Link>
-  ))
+  function renderHostVans(hostvans) {
+    const hostVansEls = hostvans.map(van => (
+     <Link to={van.id}  key={van.id} className="host-van-link-wrapper">
+       <div className="host-van-single"  key={van.id}>
+         <img src={`/src/assets/images/${van.imageUrl}`} alt={van.name} />
+         <div className="host-van-info">
+           <h3>{van.name}</h3>
+           <p>${van.price}/day</p>
+         </div>
+       </div>
+     </Link>
+   ))
+   
+   return (
+     <div className="host-vans-list">
+       <section>{hostVansEls}</section>
+     </div>              
+   )
+ }
+
+ 
 
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
-      <div className="host-vans-list">
-         <section>{hostVansEls}</section>
-      </div>
+      <Suspense fallback={ <div className='loader'><img src={`/src/assets/images/clouds-spinner.gif`} alt="loader" /></div>}>
+        <Await resolve={hostvansLoaderPromise.hostvans}>
+          {renderHostVans}
+        </Await>
+      </Suspense>
     </section>
   )
 }
