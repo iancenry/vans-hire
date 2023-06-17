46138b0ee422151d1e4d0a54cfd4d26568d38ffc
Ian Cenry <iancenry@gmail.com>
2023-06-17 21:31:50 +0300

    add Await component

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams, useLoaderData, defer } from 'react-router-dom'
+import { useSearchParams, useLoaderData, defer, Await } from 'react-router-dom'
 import { getVans } from '../../api'
 import Van from './Van'
 
@@ -7,32 +7,37 @@ export function vansLoader(){
 }
 
 const Vans = () => {  
-    const vans = useLoaderData()
+    const vansLoaderPromise = useLoaderData()
 
     const [searchParams, setSearchParams] = useSearchParams()
     const typeFilter = searchParams.get("type")
 
-    //if there is a type filter, vans that match condition are added to var else add all vans
-    const displayedVans = typeFilter ?  vans.filter(van => van.type.toLowerCase() === typeFilter ) : vans
-
-    let vansArray = displayedVans?.map( van => (
-        <Van key={van.id} id={van.id} name={van.name} price={van.price} image={van.imageUrl} type={van.type} searchParams={searchParams} />
-    ))
-
-    function serverResult(){
-        return <div className="vans">{vansArray}</div> 
-    } 
-
   return (
     <div>
         <h1 className='van-header'>Explore our van options</h1>
-        <div className="van-list-filter-buttons">
-            <button onClick={() => setSearchParams({type : "luxury"})} className={`van-type luxury ${typeFilter === 'luxury' && 'selected'}`}>Luxury</button>
-            <button onClick={() => setSearchParams({type : "simple"})} className={`van-type simple ${typeFilter === 'simple' && 'selected'}`} >Simple</button>
-            <button onClick={() => setSearchParams({type : "rugged"})} className={`van-type rugged ${typeFilter === 'rugged' && 'selected'}`}>Rugged</button>
-            {typeFilter && <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear</button>}
-        </div>
-        {serverResult()}        
+        <Await resolve={vansLoaderPromise.vans}>
+            {(vans) => {
+                // if there is a type filter, vans that match condition are added to var else add all vans
+                const displayedVans = typeFilter ?  vans.filter(van => van.type.toLowerCase() === typeFilter ) : vans
+
+                let vansArray = displayedVans?.map( van => (
+                    <Van key={van.id} id={van.id} name={van.name} price={van.price} image={van.imageUrl} type={van.type} searchParams={searchParams} />
+                ))
+
+                return (
+                    <>
+                        <div className="van-list-filter-buttons">
+                            <button onClick={() => setSearchParams({type : "luxury"})} className={`van-type luxury ${typeFilter === 'luxury' && 'selected'}`}>Luxury</button>
+                            <button onClick={() => setSearchParams({type : "simple"})} className={`van-type simple ${typeFilter === 'simple' && 'selected'}`} >Simple</button>
+                            <button onClick={() => setSearchParams({type : "rugged"})} className={`van-type rugged ${typeFilter === 'rugged' && 'selected'}`}>Rugged</button>
+                            {typeFilter && <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear</button>}
+                        </div>
+                        <div className="vans">{vansArray}</div> 
+                    </>
+                )
+            }}
+        </Await>
+               
     </div>
   )
 }
