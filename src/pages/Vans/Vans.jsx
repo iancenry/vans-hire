bdb4703d2209bb40d086b85b8d45160fb1f365c4
Ian Cenry <iancenry@gmail.com>
2023-06-17 21:35:55 +0300

    Await vans refractor

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -12,30 +12,32 @@ const Vans = () => {
     const [searchParams, setSearchParams] = useSearchParams()
     const typeFilter = searchParams.get("type")
 
+    function renderVanElements(vans){
+        // if there is a type filter, vans that match condition are added to var else add all vans
+        const displayedVans = typeFilter ?  vans.filter(van => van.type.toLowerCase() === typeFilter ) : vans
+
+        let vansArray = displayedVans?.map( van => (
+            <Van key={van.id} id={van.id} name={van.name} price={van.price} image={van.imageUrl} type={van.type} searchParams={searchParams} />
+        ))
+
+        return (
+            <>
+                <div className="van-list-filter-buttons">
+                    <button onClick={() => setSearchParams({type : "luxury"})} className={`van-type luxury ${typeFilter === 'luxury' && 'selected'}`}>Luxury</button>
+                    <button onClick={() => setSearchParams({type : "simple"})} className={`van-type simple ${typeFilter === 'simple' && 'selected'}`} >Simple</button>
+                    <button onClick={() => setSearchParams({type : "rugged"})} className={`van-type rugged ${typeFilter === 'rugged' && 'selected'}`}>Rugged</button>
+                    {typeFilter && <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear</button>}
+                </div>
+                <div className="vans">{vansArray}</div> 
+            </>
+        )
+    }
+    
   return (
     <div>
         <h1 className='van-header'>Explore our van options</h1>
         <Await resolve={vansLoaderPromise.vans}>
-            {(vans) => {
-                // if there is a type filter, vans that match condition are added to var else add all vans
-                const displayedVans = typeFilter ?  vans.filter(van => van.type.toLowerCase() === typeFilter ) : vans
-
-                let vansArray = displayedVans?.map( van => (
-                    <Van key={van.id} id={van.id} name={van.name} price={van.price} image={van.imageUrl} type={van.type} searchParams={searchParams} />
-                ))
-
-                return (
-                    <>
-                        <div className="van-list-filter-buttons">
-                            <button onClick={() => setSearchParams({type : "luxury"})} className={`van-type luxury ${typeFilter === 'luxury' && 'selected'}`}>Luxury</button>
-                            <button onClick={() => setSearchParams({type : "simple"})} className={`van-type simple ${typeFilter === 'simple' && 'selected'}`} >Simple</button>
-                            <button onClick={() => setSearchParams({type : "rugged"})} className={`van-type rugged ${typeFilter === 'rugged' && 'selected'}`}>Rugged</button>
-                            {typeFilter && <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear</button>}
-                        </div>
-                        <div className="vans">{vansArray}</div> 
-                    </>
-                )
-            }}
+            {renderVanElements}
         </Await>
                
     </div>
