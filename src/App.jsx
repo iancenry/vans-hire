9afcb0ea82f0d31ab11e9a678ee5e234f85e14f0
Ian Cenry <iancenry@gmail.com>
2023-06-17 23:21:54 +0300

    add error element to other routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path="login" element={<Login/>} loader={loginLoader} action={loginAction} />
     <Route path='about' element={<About />} />
     <Route path='vans' element={<Vans />} loader={vansLoader} errorElement={<Error />}/>
-    <Route path='vans/:id' element={<VanDetail />} loader={vanDetailLoader} />
+    <Route path='vans/:id' element={<VanDetail />} loader={vanDetailLoader} errorElement={<Error />} />
             
     <Route path='host' element={<HostLayout />} >
       <Route index element={<Dashboard />} loader={async({request}) => await requireAuth(request)}/>
       <Route path="income" element={<Income />} loader={async({request}) => await requireAuth(request)}/>
       <Route path="reviews" element={<Reviews />} loader={async({request}) => await requireAuth(request)}/>
-      <Route path="vans" element={<HostVans />} loader={hostVansLoader}/>
+      <Route path="vans" element={<HostVans />} loader={hostVansLoader} errorElement={<Error />}/>
 
-      <Route path="vans/:id" element={<HostVanDetail />} loader={hostVanDetailLoader}>
+      <Route path="vans/:id" element={<HostVanDetail />} loader={hostVanDetailLoader} errorElement={<Error />}>
         <Route index element={<HostVanInfo />} loader={async({request}) => await requireAuth(request)}/>
         <Route path="pricing" element={<HostVanPricing />} loader={async({request}) => await requireAuth(request)}/>
         <Route path="photos" element={<HostVanPhotos />} loader={async({request}) => await requireAuth(request)}/>
